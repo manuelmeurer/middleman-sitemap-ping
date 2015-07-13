8ec7dfb79210f479cb84d3125bb4613babe734d6
Manuel Meurer <manuel@krautcomputing.com>
2015-07-12 20:50:13 -0400

    dev

--- lib/middleman-sitemap-ping.rb
+++ lib/middleman-sitemap-ping.rb
@@ -2,12 +2,16 @@ require 'middleman-core'
 
 module Middleman
   class SitemapPing < Extension
-    URLS = %w(
-      http://www.google.com/webmasters/tools/ping?sitemap=SITEMAP
-      http://search.yahooapis.com/SiteExplorerService/V1/ping?sitemap=SITEMAP
-      http://submissions.ask.com/ping?sitemap=SITEMAP
-      http://webmaster.live.com/ping.aspx?siteMap=SITEMAP
-    )
+    SERVICES = {
+      google: 'http://www.google.com/webmasters/tools/ping?sitemap=SITEMAP_URL',
+      bing:   'http://www.bing.com/ping?sitemap=SITEMAP_URL'
+    }
+
+    SERVICES.each_key do |service|
+      option "ping_#{service}".to_sym, true, "Ping #{service.capitalize}?"
+    end
+    option :host,         nil,           'The host of your website'
+    option :sitemap_file, 'sitemap.xml', 'The name of your sitemap file'
 
     def initialize(app, options_hash = {})
       super
@@ -16,14 +20,18 @@ module Middleman
     end
 
     def after_build(builder)
-      URLS.each do |url|
-        url.sub! 'SITEMAP', '...'
-        puts "Pinging #{url}"
+      raise 'Please set the `host` option for the sitemap ping extension!' unless host = options.host
+      host = "http://#{host}" unless host =~ %r(\Ahttps?://)
+      sitemap_url = File.join(host, options.sitemap_file)
+      SERVICES.each do |service, url|
+        next unless options.send("ping_#{service}")
+        url.sub! /SITEMAP_URL\z/, CGI.escape(sitemap_url)
+        builder.say "Pinging #{url}"
         open url do |f|
           if f.status[0] == '200'
-            puts 'SUCCESS!'
+            builder.say_status :success, 'SUCCESS!', :green
           else
-            puts "ERROR: #{f.status[0]}"
+            builder.say_status :error, "ERROR: #{f.status[0]}", :red
           end
         end
       end
